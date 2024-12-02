92878e4463d109965aed5b0e3d4e138498150186
Lars Baumeister <7094093+larsbaumeister@users.noreply.github.com.>
2024-12-02 18:30:19 +0100

    day 2

--- src/main/java/de/baumeister/day2/DayTwoPartOne.java
+++ src/main/java/de/baumeister/day2/DayTwoPartOne.java
@@ -5,14 +5,12 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class DayTwoPartOne {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
 
         var count = Files.readAllLines(Path.of(DayTwoPartOne.class.getResource("/day2/input.txt").toURI())).stream()
-//        var count = Stream.of("7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9")
                 .map(line -> Arrays.stream(line.split("\s+")).mapToInt(Integer::parseInt).toArray())
                 .filter(numbers -> isAscending(numbers) || isDescending(numbers))
                 .count();
@@ -22,22 +20,19 @@ public class DayTwoPartOne {
     }
 
     private static boolean isAscending(int[] numbers) {
-        var prevElem = numbers[0];
-        for (var i = 1; i < numbers.length; i++) {
-            var distance = Math.abs(prevElem - numbers[i]);
-            if (prevElem >= numbers[i] || distance > 3) {
-                return false;
-            }
-            prevElem = numbers[i];
-        }
-        return true;
+        return isDescOrAsc(false, numbers);
     }
 
     private static boolean isDescending(int[] numbers) {
+        return isDescOrAsc(true, numbers);
+    }
+
+    private static boolean isDescOrAsc(boolean desc, int[] numbers) {
         var prevElem = numbers[0];
         for (var i = 1; i < numbers.length; i++) {
             var distance = Math.abs(prevElem - numbers[i]);
-            if (prevElem <= numbers[i] || distance > 3) {
+            var currentElem = numbers[i];
+            if ((desc ? prevElem <= currentElem : prevElem >= currentElem) || distance > 3) {
                 return false;
             }
             prevElem = numbers[i];
--- src/main/java/de/baumeister/day2/DayTwoPartTwo.java
+++ src/main/java/de/baumeister/day2/DayTwoPartTwo.java
@@ -6,27 +6,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class DayTwoPartTwo {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
          var count = Files.readAllLines(Path.of(DayTwoPartOne.class.getResource("/day2/input.txt").toURI())).stream()
-                .map(line -> new LinkedList<Integer>(Arrays.stream(line.split("\s+")).map(Integer::parseInt).toList()))
+                .map(line -> new LinkedList<>(Arrays.stream(line.split("\s+")).map(Integer::parseInt).toList()))
                 .filter(numbers -> isAscending(numbers) || isDescending(numbers))
                 .count();
 
         System.out.println(count);
     }
 
-    private static boolean isAscending(LinkedList<Integer> numbers) {
+    private static boolean isAscending(List<Integer> numbers) {
         return isDescOrAsc(false, numbers, 1);
     }
 
-    private static boolean isDescending(LinkedList<Integer> numbers) {
+    private static boolean isDescending(List<Integer> numbers) {
         return isDescOrAsc(true, numbers, 1);
     }
 
-    private static boolean isDescOrAsc(boolean desc, LinkedList<Integer> numbers, int allowedRemovals) {
+    private static boolean isDescOrAsc(boolean desc, List<Integer> numbers, int allowedRemovals) {
         var prevElem = numbers.getFirst();
         for (var i = 1; i < numbers.size(); i++) {
             var currentElem = numbers.get(i);
