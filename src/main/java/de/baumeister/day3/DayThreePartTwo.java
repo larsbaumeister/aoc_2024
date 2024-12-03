faedbf437ed91b7320b9eed28ed3592b816bf82b
Lars Baumeister <7094093+larsbaumeister@users.noreply.github.com.>
2024-12-03 20:11:06 +0100

    day 3

--- src/main/java/de/baumeister/day3/DayThreePartTwo.java
+++ src/main/java/de/baumeister/day3/DayThreePartTwo.java
@@ -10,40 +10,31 @@ public class DayThreePartTwo {
 
     public static void main(String[] args) throws Exception {
         var code = Files.readString(Path.of(DayTwoPartOne.class.getResource("/day3/input.txt").toURI()));
-        var res = CodeRunner.runCode(code);
-        System.out.println(res);
-    }
 
-    static class CodeRunner {
-        boolean active = true;
-        StringBuilder currentWord = new StringBuilder();
-        Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+        var active = true;
+        var currentWord = new StringBuilder();
+        var pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+
         long sum = 0;
-
-        public static long runCode(String code) {
-            var runner = new CodeRunner();
-            runner.run(code);
-            return runner.sum;
-        }
-
-        private void run(String code) {
-            for (var i = 0; i < code.length(); i++) {
-                var c = code.charAt(i);
-                currentWord.append(c);
-                if (currentWord.toString().endsWith("do()")) {
-                    active = true;
+        for (var i = 0; i < code.length(); i++) {
+            var c = code.charAt(i);
+            currentWord.append(c);
+            if (currentWord.toString().endsWith("do()")) {
+                active = true;
+                currentWord = new StringBuilder();
+            } else if (currentWord.toString().endsWith("don't()")) {
+                active = false;
+                currentWord = new StringBuilder();
+            } else if (active) {
+                var matcher = pattern.matcher(currentWord);
+                while (matcher.find()) {
+                    sum += (long) Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
                     currentWord = new StringBuilder();
-                } else if (currentWord.toString().endsWith("don't()")) {
-                    active = false;
-                    currentWord = new StringBuilder();
-                } else if (active) {
-                    var matcher = pattern.matcher(currentWord);
-                    while (matcher.find()) {
-                        sum += (long) Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
-                        currentWord = new StringBuilder();
-                    }
                 }
             }
         }
+
+        System.out.println(sum);
     }
+
 }
