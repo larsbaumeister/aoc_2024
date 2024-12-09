abd53d0d800e1ec7b6b5d4c9b1370b9bc289b05c
Lars Baumeister <7094093+larsbaumeister@users.noreply.github.com.>
2024-12-09 14:26:39 +0100

    day 9

---
+++ src/main/java/de/baumeister/day9/DayNinePartOne.java
@@ -0,0 +1,95 @@
+package de.baumeister.day9;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class DayNinePartOne {
+
+    public static void main(String[] args) throws Exception {
+        var s = Files.readString(Path.of(DayNinePartOne.class.getResource("/day9/input.txt").toURI()));
+        var fs = FileSystem.from(s);
+        fs.shiftLeft();
+        var sum = fs.checksum();
+        System.out.println(sum);
+    }
+
+    record FileSystem(LinkedList<FileBlock> blocks) {
+
+        public static FileSystem from(String s) {
+            var list = new LinkedList<FileBlock>();
+            for (var i = 0; i < s.length(); i++) {
+                var isEmpty = i % 2 != 0;
+                var block = new FileBlock(isEmpty ? -1 : i / 2, Character.getNumericValue(s.charAt(i)), isEmpty, s.charAt(i), false);
+                if (block.size > 0)
+                    list.add(block);
+            }
+            return new FileSystem(list);
+        }
+
+        public void shiftLeft() {
+            for (var block : new ArrayList<>(blocks).reversed()) {
+                if (!block.isEmpty) {
+                    var shiftPerformed = shiftBlockLeft(block);
+                    if (!shiftPerformed)
+                        break;
+                }
+            }
+        }
+
+        private boolean shiftBlockLeft(FileBlock block) {
+            var cancelled = false;
+            var remaining = block.size;
+            while (remaining > 0) {
+                var nextBlock = getNextEmptyBlock();
+                var nextBlockIndex = blocks.indexOf(nextBlock);
+                if (nextBlockIndex >= blocks.indexOf(block)) {
+                    if (remaining == block.size) {
+                        return false;
+                    }
+                    cancelled = true;
+
+                }
+
+                var toMove = Math.min(remaining, nextBlock.size);
+                var newBlock = new FileBlock(block.id, toMove, false, block.value, true);
+                blocks.set(nextBlockIndex, newBlock);
+
+                if (toMove < nextBlock.size) {
+                    var newEmptyBlock = new FileBlock(nextBlock.id, nextBlock.size - toMove, true, nextBlock.value, false);
+                    blocks.add(nextBlockIndex + 1, newEmptyBlock);
+                }
+
+                blocks.add(new FileBlock(nextBlock.id, toMove, true, nextBlock.value, false));
+
+                remaining -= toMove;
+            }
+            blocks.remove(block);
+            return !cancelled;
+        }
+
+        private FileBlock getNextEmptyBlock() {
+            for (var block : blocks) {
+                if (block.isEmpty) {
+                    return block;
+                }
+            }
+            return null;
+        }
+
+        private long checksum() {
+            long sum = 0;
+            var idx = 0;
+            for (var block : blocks) {
+                for (var i = 0; i < block.size; i++) {
+                    sum += Math.max(0, (long) block.id * idx++);
+                }
+            }
+            return sum;
+        }
+
+    }
+
+    record FileBlock(int id, int size, boolean isEmpty, char value, boolean isCopy) {}
+}
---
+++ src/main/java/de/baumeister/day9/DayNinePartTwo.java
@@ -0,0 +1,69 @@
+package de.baumeister.day9;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class DayNinePartTwo {
+    public static void main(String[] args) throws Exception {
+        var s = Files.readString(Path.of(DayNinePartOne.class.getResource("/day9/input.txt").toURI()));
+        var fs = FileSystem.from(s);
+        fs.shiftLeft();
+        var sum = fs.checksum();
+        System.out.println(sum);
+    }
+
+    record FileSystem(LinkedList<FileBlock> blocks) {
+
+        public static FileSystem from(String s) {
+            var list = new LinkedList<FileBlock>();
+            for (var i = 0; i < s.length(); i++) {
+                var isEmpty = i % 2 != 0;
+                var block = new FileBlock(isEmpty ? -1 : i / 2, Character.getNumericValue(s.charAt(i)), isEmpty, s.charAt(i), false);
+                if (block.size > 0)
+                    list.add(block);
+            }
+            return new FileSystem(list);
+        }
+
+        public void shiftLeft() {
+            for (var block : new ArrayList<>(blocks).reversed()) {
+                if (!block.isEmpty) {
+                    var emptyBlock = getNextEmptyBlock(block.size);
+                    if (emptyBlock == null || blocks.indexOf(emptyBlock) > blocks.indexOf(block)) {
+                        continue;
+                    }
+                    if (emptyBlock.size > block.size) {
+                        blocks.add(blocks.indexOf(emptyBlock) + 1, new FileBlock(-1, emptyBlock.size - block.size, true, ' ', false));
+                    }
+                    blocks.set(blocks.indexOf(emptyBlock), new FileBlock(block.id, block.size, false, block.value, true));
+                    blocks.set(blocks.indexOf(block), new FileBlock(-1, block.size, true, block.value, false));
+                }
+            }
+        }
+
+        private FileBlock getNextEmptyBlock(int minSpace) {
+            for (var block : blocks) {
+                if (block.isEmpty && block.size >= minSpace) {
+                    return block;
+                }
+            }
+            return null;
+        }
+
+        private long checksum() {
+            long sum = 0;
+            var idx = 0;
+            for (var block : blocks) {
+                for (var i = 0; i < block.size; i++) {
+                    sum += Math.max(0, (long) block.id * idx++);
+                }
+            }
+            return sum;
+        }
+
+    }
+
+    record FileBlock(int id, int size, boolean isEmpty, char value, boolean isCopy) {}
+}
